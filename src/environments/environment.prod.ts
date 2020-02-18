8e09e26736cd10b3dd1240e80d469c2b9bd4f004
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2020-02-17 22:48:28 -0600

    Eliminacion de la API Key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBpjceBuA4tpSyO5SXCQWrMMp9Kv3C4UdU',
-    authDomain: 'pruebas-354cc.firebaseapp.com',
-    databaseURL: 'https://pruebas-354cc.firebaseio.com/',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
     projectId: 'pruebas-354cc',
-    storageBucket: 'gs://pruebas-354cc.appspot.com/',
-    messagingSenderId: '571399831233'
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: 'AIzaSyBpjceBuA4tpSyO5SXCQWrMMp9Kv3C4UdU',
-    authDomain: 'pruebas-354cc.firebaseapp.com',
-    databaseURL: 'https://pruebas-354cc.firebaseio.com/',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
     projectId: 'pruebas-354cc',
-    storageBucket: 'gs://pruebas-354cc.appspot.com/',
-    messagingSenderId: '571399831233'
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
